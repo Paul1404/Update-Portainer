d68a97fbc4e6bcfeb93c7d4092047cfe224b9c01
Paul <97813342+Paul1404@users.noreply.github.com>
2023-09-09 00:06:12 +0200

    Refactor Portainer update script for enhanced readability and error handling
    - Modularized the script by introducing functions for main tasks.
    - Implemented error checks after critical docker commands.
    - Improved comments for clarity and context.
    - Streamlined the user prompts and response handling for version inputs.

--- update.sh
+++ update.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 
-# Name of the existing Portainer container
 CONTAINER_NAME="Portainer"
 
-# Ask user for confirmation
+# Check if a command was successful
+check_command_success() {
+    if [[ $? -ne 0 ]]; then
+        echo "Error encountered. Exiting."
+        exit 1
+    fi
+}
+
+# Stop and remove existing container
+remove_existing_container() {
+    echo "Stopping existing Portainer container..."
+    docker stop $CONTAINER_NAME
+    check_command_success
+
+    echo "Removing existing Portainer container..."
+    docker rm $CONTAINER_NAME
+    check_command_success
+}
+
+# Pull and run the latest image
+update_portainer() {
+    local IMAGE_NAME="$1"
+    local PORTS="$2"
+
+    echo "Pulling the latest Portainer image..."
+    docker pull $IMAGE_NAME
+    check_command_success
+
+    echo "Running the new Portainer container..."
+    docker run -d -p $PORTS:9000 --name=$CONTAINER_NAME -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data $IMAGE_NAME
+    check_command_success
+}
+
+# Main script
 read -p "This script will stop and remove your existing Portainer container and create a new one with the latest image. Do you want to proceed? (y/n) " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # Ask user for the version of Portainer
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     read -p "Are you using Portainer CE or EE? (ce/ee) " version
     echo
 
-    if [[ $version == "ce" ]]
-    then
+    IMAGE_NAME=""
+    if [[ $version == "ce" ]]; then
         IMAGE_NAME="portainer/portainer-ce:latest"
-    elif [[ $version == "ee" ]]
-    then
+    elif [[ $version == "ee" ]]; then
         IMAGE_NAME="portainer/portainer-ee:latest"
     else
         echo "Invalid version. Please enter either 'ce' or 'ee'."
         exit 1
     fi
 
-    # Get existing Portainer ports
     PORTS=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9000/tcp") 0).HostPort}}' $CONTAINER_NAME)
-    if [ -z "$PORTS" ]
-    then
+    if [ -z "$PORTS" ]; then
         echo "Unable to find port mapping for Portainer. Please make sure that Portainer is running and that port 9000 is mapped to a host port."
         exit 1
     fi
 
-    # Stop the existing Portainer container
-    docker stop $CONTAINER_NAME
+    remove_existing_container
+    update_portainer $IMAGE_NAME $PORTS
 
-    # Remove the existing Portainer container
-    docker rm $CONTAINER_NAME
-
-    # Pull the latest image of Portainer
-    docker pull $IMAGE_NAME
-
-    # Run a new Portainer container with the latest image
-    docker run -d -p $PORTS:9000 --name=$CONTAINER_NAME -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data $IMAGE_NAME
-
-    # Print the status of the new Portainer container
+    echo "Status of the new Portainer container:"
     docker ps -a | grep $CONTAINER_NAME
+
 else
     echo "Update canceled."
     exit 1
